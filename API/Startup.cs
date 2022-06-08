615b45750e8aa4c7049072afa9702d9fcc11a681
Vladislav Smihula <vladislav.smihula@nxtsoft.sk>
2022-06-08 11:23:51 +0200

    Chnages in security headers

--- API/Startup.cs
+++ API/Startup.cs
@@ -50,10 +50,34 @@ namespace API
       //We have created this custom middleware to help us handle exception eg. in Delete.cs when activity to delete doesn't exist
       app.UseMiddleware<ExceptionMiddleware>();
 
+      //Security settings via package NWebsec.AspNetCore.Middleware
+      app.UseXContentTypeOptions();
+      app.UseReferrerPolicy(opt => opt.NoReferrer());
+      app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+      app.UseXfo(opt => opt.Deny());
+      app.UseCsp(opt => opt
+            .BlockAllMixedContent()
+            .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "https://cdn.jsdelivr.net"))
+            .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "https://cdn.jsdelivr.net", "data:"))
+            .FormActions(s => s.Self())
+            .FrameAncestors(s => s.Self())
+            .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+            .ScriptSources(s => s.Self())
+      );
+
       if (env.IsDevelopment())
       {
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+      }
+
+      else
+      {
+        app.Use(async (context, next) =>
+        {
+          context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+          await next.Invoke();
+        });
       }
 
       //if we are not ussing https in development then we can comment this out
