4a9a5206c2756a4bd45463cfac687af1535a6ad1
Vladislav Smihula <vladislav.smihula@nxtsoft.sk>
2022-07-20 11:41:56 +0200

    last commit before deployment

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,7 +5,6 @@ import "react-toastify/dist/ReactToastify.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
-import reportWebVitals from "./reportWebVitals";
 import { store, StoreContext } from "./app/stores/store";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
@@ -23,8 +22,3 @@ ReactDOM.render(
   </StoreContext.Provider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
